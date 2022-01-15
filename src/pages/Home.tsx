1317a62b6108e6a861393283728608669f5a806c
YoungDevDotMp3 <brkshen@gmail.com>
2022-01-15 10:36:04 -0300

    finish test

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,23 +1,37 @@
-import React, { useState } from 'react';
-import { StyleSheet, View } from 'react-native';
+import React, { useState } from "react";
+import { StyleSheet, View } from "react-native";
 
-import { Header } from '../components/Header';
-import { Task, TasksList } from '../components/TasksList';
-import { TodoInput } from '../components/TodoInput';
+import uuid from "react-native-uuid";
+
+import { Header } from "../components/Header";
+import { Task, TasksList } from "../components/TasksList";
+import { TodoInput } from "../components/TodoInput";
 
 export function Home() {
   const [tasks, setTasks] = useState<Task[]>([]);
 
   function handleAddTask(newTaskTitle: string) {
-    //TODO - add new task
+    const newTask: Task = {
+      id: String(uuid.v4()),
+      title: newTaskTitle,
+      done: false,
+    };
+
+    setTasks((oldTasks) => [...oldTasks, newTask]);
   }
 
-  function handleToggleTaskDone(id: number) {
-    //TODO - toggle task done if exists
+  function handleToggleTaskDone(id: string) {
+    const newTasks = tasks.map((task) => {
+      return task.id === id ? { ...task, done: true } : task;
+    });
+
+    setTasks(newTasks);
   }
 
-  function handleRemoveTask(id: number) {
-    //TODO - remove task from state
+  function handleRemoveTask(id: string) {
+    const newTasks = tasks.filter((task) => task.id !== id);
+
+    setTasks(newTasks);
   }
 
   return (
@@ -26,18 +40,18 @@ export function Home() {
 
       <TodoInput addTask={handleAddTask} />
 
-      <TasksList 
-        tasks={tasks} 
+      <TasksList
+        tasks={tasks}
         toggleTaskDone={handleToggleTaskDone}
-        removeTask={handleRemoveTask} 
+        removeTask={handleRemoveTask}
       />
     </View>
-  )
+  );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#EBEBEB'
-  }
-})
+    backgroundColor: "#EBEBEB",
+  },
+});
